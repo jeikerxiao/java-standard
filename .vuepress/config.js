d13593cb3641dcf09a3ca9d414ab05e5f3b3bd2d
jeikerxiao <jeiker@126.com>
2018-11-06 18:49:36 +0800

    init project
    init project

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Html', link: 'http://html.tmqyt.com/dist/' },
+        ],
+        sidebar: 'auto'
+    }
+}
